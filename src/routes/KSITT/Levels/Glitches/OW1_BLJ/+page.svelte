abefc0e12b5f7831f9568be74a751b9d808e7e80
ChiaraSM64 <chiarasm64@gmail.com>
2025-09-22 19:02:45 +0200

    added note for 1st blj

--- src/routes/KSITT/Levels/Glitches/OW1_BLJ/+page.svelte
+++ src/routes/KSITT/Levels/Glitches/OW1_BLJ/+page.svelte
@@ -7,7 +7,10 @@
 
 <h2>Strats</h2>
 
-<h3>Beginner</h3>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/9nBuUxq4sC8?si=g709JLWaVJ3T27we" title="YouTube video player" 
     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<p>
+        <em>If you have enough speed, you can directly clip through the door without kicking, which saves arround 3sec</em>
+    </p>
