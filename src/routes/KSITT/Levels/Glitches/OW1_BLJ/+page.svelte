5ffc488cdbfb49a9d2009abfa17cc88fff47a1e2
ChiaraSM64 <chiarasm64@gmail.com>
2025-09-22 18:54:48 +0200

    added ksitt 1st blj video

---
+++ src/routes/KSITT/Levels/Glitches/OW1_BLJ/+page.svelte
@@ -0,0 +1,13 @@
+<h1>RTA Guide/Overworld 1</h1>
+
+<p>
+    Overworld 1 is the starting area of the hack. only 1 Burger is required to enter the 2nd overworld. 
+    However, you can enter the door without collecting any burgers by doing a blj on the bin.
+</p>
+
+<h2>Strats</h2>
+
+<h3>Beginner</h3>
+    <iframe width="560" height="315" src="" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
