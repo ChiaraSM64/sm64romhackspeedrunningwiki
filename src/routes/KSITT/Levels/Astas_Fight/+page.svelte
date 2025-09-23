1a8018cb1a56ebfa2441ddc66de18df80e707881
ChiaraSM64 <chiarasm64@gmail.com>
2025-09-23 19:47:54 +0200

    finished the 0 Star part

---
+++ src/routes/KSITT/Levels/Astas_Fight/+page.svelte
@@ -0,0 +1,20 @@
+<h1>RTA Guide/Asta's Fight</h1>
+
+<p>
+    Asta's Fight is the 2nd part of the bowser course (Asta's Room). 
+    You can acess it by going into the pipe at the end of Asta's Room.
+</p>
+
+<h2>Strats</h2>
+
+<h3>Beginner/Intermediate</h3>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/QpMhkQEVIoI?si=FYk2SXikCTGmASwI" title="YouTube video player" 
+frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<h3>Advanced</h3>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/S-PWz9R_BiU?si=XLtO0j01rZTBMHHV" title="YouTube video player" 
+frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
